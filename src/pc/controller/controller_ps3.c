0c0e8792d7471f0bb733c536e262fd501fca04e1
Julien de Castelnau <decastelnau@wisc.edu>
2020-09-28 15:32:44 -0500

    C-buttons & Z-trigger rebind

--- src/pc/controller/controller_ps3.c
+++ src/pc/controller/controller_ps3.c
@@ -31,15 +31,29 @@ static void controller_ps3_read(OSContPad *pad) {
             ioPadGetData(i, &paddata);
 
             if (paddata.BTN_START) pad->button |= START_BUTTON;
-            if (paddata.BTN_L1) pad->button |= Z_TRIG;
-            if (paddata.BTN_L1) pad->button |= Z_TRIG;
+            // Uncomment to allow either L2 or R2
+            // if (paddata.BTN_L2 || paddata.BTN_R2) pad->button |= Z_TRIG;
+            // Comment to remove
+            if (paddata.BTN_L2) pad->button |= Z_TRIG;
+            if (paddata.BTN_R1) pad->button |= L_TRIG;
+            if (paddata.BTN_R1) pad->button |= R_TRIG;
             if (paddata.BTN_CROSS) pad->button |= A_BUTTON;
             if (paddata.BTN_SQUARE) pad->button |= B_BUTTON;
 
+            const uint8_t rstick_h = ((uint8_t)paddata.ANA_R_H);
+            const uint8_t rstick_v = ((uint8_t)paddata.ANA_R_V);
+            if (rstick_h < 0x40) pad->button |= L_CBUTTONS;
+            if (rstick_h > 0xC0) pad->button |= R_CBUTTONS;
+            if (rstick_v < 0x40) pad->button |= D_CBUTTONS;
+            if (rstick_v > 0xC0) pad->button |= U_CBUTTONS;
+
+            // Uncomment to use D-pads instead
+            /*
             if (paddata.BTN_LEFT) pad->button |= L_CBUTTONS;
             if (paddata.BTN_RIGHT) pad->button |= R_CBUTTONS;
             if (paddata.BTN_UP) pad->button |= U_CBUTTONS;
             if (paddata.BTN_DOWN) pad->button |= D_CBUTTONS;
+            */
 
             const int16_t stick_h = ((uint8_t)paddata.ANA_L_H) - 0x80;
             const int16_t stick_v = 0x80 - ((uint8_t)paddata.ANA_L_V);
